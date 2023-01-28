5bbf03286c8a3adde240989e1e0a574199ace508
Kim Hyeri <37619441+m3rri@users.noreply.github.com>
2023-01-28 19:21:12 +0900

    Update lesson4-max-counters.java

--- lesson4-max-counters.java
+++ lesson4-max-counters.java
@@ -38,3 +38,48 @@ class Solution {
             .getAsInt();
     }
 }
+
+//O(N*M) -> 55%
+class Solution {
+    private int[] counters;
+
+    public int[] solution(int N, int[] A) {
+        int max = 0;
+        initCounters(N);
+
+        for(int a : A){
+            if(a<=N){
+                increase(a);
+            }else{
+                max += getMax();
+                initCounters(N);
+            }
+        }
+
+        maxCounter(max);
+
+        return this.counters;
+    }
+
+    private void initCounters(int N){
+        this.counters = new int[N];
+    }
+
+    private void increase(int X){
+        this.counters[X-1]++;
+    }
+
+    private void maxCounter(int max){
+        this.counters = IntStream
+            .of(this.counters)
+            .map(a->a+max)
+            .toArray();
+    }
+
+    private int getMax(){
+        return IntStream
+            .of(this.counters)
+            .max()
+            .getAsInt();
+    }
+}
