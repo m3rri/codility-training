4fad3e8a92180dfab0268b988918e2dfb6a1bd36
Kim Hyeri <37619441+m3rri@users.noreply.github.com>
2023-01-28 19:49:09 +0900

    Update lesson4-max-counters.java

--- lesson4-max-counters.java
+++ lesson4-max-counters.java
@@ -1,43 +1,46 @@
 import java.util.stream.IntStream;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
-//O(N*M) -> 55%
+//O(N + M)
 class Solution {
-    private int[] counters;
-
     public int[] solution(int N, int[] A) {
-        this.counters = new int[N];
+        int[] counters = new int[N];
+        int max = 0;
+        Map<Integer, Integer> map = new HashMap<>();
 
         for(int a : A){
             if(a<=N){
-                increase(a);
+                Integer value = map.get(a);
+
+                if(value!=null){
+                    value++;
+                }else{
+                    value=1;
+                }
+
+                map.put(a, value);
             }else{
-                maxCounter();
+                Collection<Integer> values = map.values();
+                if(values.size()>0){
+                    max += Collections.max(values);
+                    map = new HashMap<>();
+                }
             }
         }
 
-        return this.counters;
-    }
+        for(int i=0; i<N; i++){
+            Integer value = map.get(i+1);
+            counters[i] = max + (value==null ? 0 : value);
+        }
 
-    private void increase(int X){
-        this.counters[X-1]++;
-    }
-
-    private void maxCounter(){
-        int max = getMax();
-        
-        this.counters = IntStream
-            .of(this.counters)
-            .map(a->max)
-            .toArray();
-    }
-
-    private int getMax(){
-        return IntStream
-            .of(this.counters)
-            .max()
-            .getAsInt();
+        return counters;
     }
 }
+
+import java.util.stream.IntStream;
 
 //O(N*M) -> 55%
 class Solution {
@@ -73,6 +76,45 @@ class Solution {
         this.counters = IntStream
             .of(this.counters)
             .map(a->a+max)
+            .toArray();
+    }
+
+    private int getMax(){
+        return IntStream
+            .of(this.counters)
+            .max()
+            .getAsInt();
+    }
+}
+
+//O(N*M) -> 55%
+class Solution {
+    private int[] counters;
+
+    public int[] solution(int N, int[] A) {
+        this.counters = new int[N];
+
+        for(int a : A){
+            if(a<=N){
+                increase(a);
+            }else{
+                maxCounter();
+            }
+        }
+
+        return this.counters;
+    }
+
+    private void increase(int X){
+        this.counters[X-1]++;
+    }
+
+    private void maxCounter(){
+        int max = getMax();
+        
+        this.counters = IntStream
+            .of(this.counters)
+            .map(a->max)
             .toArray();
     }
 
