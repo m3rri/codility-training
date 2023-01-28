a4a15fee3615a7ab9f8407a5f9ca5eb63554a313
Kim Hyeri <37619441+m3rri@users.noreply.github.com>
2023-01-28 16:16:17 +0900

    Update lesson4-frog-river-one.java

--- lesson4-frog-river-one.java
+++ lesson4-frog-river-one.java
@@ -22,3 +22,28 @@ class Solution {
         return second;
     }
 }
+
+import java.util.Map;
+import java.util.HashMap;
+
+//O(N)
+class Solution {
+    public int solution(int X, int[] A) {
+        int second = -1;
+
+        if(A.length>=X){
+            Map<Integer, Boolean> leavesPosition = new HashMap();
+
+            for(int i=0; i<A.length; i++){
+                leavesPosition.put(A[i], true);
+                
+                if(i>=(X-1) && leavesPosition.keySet().size()==X){ //i 체크 별 차이 없음
+                    second = i;
+                    break;
+                }
+            }
+        }
+
+        return second;
+    }
+}
