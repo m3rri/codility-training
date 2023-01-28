c9103b772353e9c320e125103881c9afdc693ba0
Kim Hyeri <37619441+m3rri@users.noreply.github.com>
2023-01-28 22:12:44 +0900

    Update lesson4-missing-integer.java

--- lesson4-missing-integer.java
+++ lesson4-missing-integer.java
@@ -58,3 +58,56 @@ class Solution {
         return result;
     }
 }
+
+//짧ver
+import java.util.stream.IntStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Iterator;
+import java.util.Set;
+
+class Solution {
+    public int solution(int[] A) {
+        int result = 0;
+        int M = A.length;
+
+        if(M<=1){
+            result = M==1 && A[0]==1 ? 2 : 1;
+        }else{
+            List<Integer> list = new ArrayList<>();
+            IntStream.of(A).forEach(a->{
+                if(a>0){
+                    list.add(a);
+                }
+            });
+
+            M = list.size();
+
+            if(M<=1){
+                result = M==1 && list.get(0)==1 ? 2 : 1;
+            }else{
+                list.sort((a, b)->a-b);
+
+                for(int i=0; i<M; i++){
+                    if(i==0){
+                        if(list.get(i)>1){
+                            result = 1;
+                            break;
+                        }
+                    }else if(list.get(i)-list.get(i-1)>1){
+                        result = list.get(i-1)+1;
+                        break;
+                    }
+
+                    if(i==M-1){
+                        result = list.get(i)+1;
+                    }
+                }
+            }
+        }
+        
+        return result;
+    }
+}
